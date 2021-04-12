9fb1d78dbb85c3fe67a27a77a2f8c000df1fb623
Armel Ric Manglo <armelricmanglo@gmail.com>
2021-04-12 17:55:42 +0800

    Activity 2

--- app/src/main/java/com/example/activity2/MainActivity.java
+++ app/src/main/java/com/example/activity2/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     EditText text_num1,text_num2;
-    Button button_addition,button_subtraction;
+    Button button_addition, button_subtraction, button_multiplication, button_division, button_clear;
     TextView text_answer;
 
     @Override
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
         text_answer = findViewById(R.id.txt_answer);
         button_addition = findViewById(R.id.btn_addition);
         button_subtraction = findViewById(R.id.btn_subtraction);
+        button_multiplication = findViewById(R.id.btn_multiplication);
+        button_division = findViewById(R.id.btn_division);
+        button_clear = findViewById(R.id.btn_clear);
         text_num1 = findViewById(R.id.num_input1);
         text_num2 = findViewById(R.id.num_input2);
 
@@ -46,6 +49,39 @@ public class MainActivity extends AppCompatActivity {
                 double result = first_value - second_value;
 
                 text_answer.setText(String.valueOf(result));
+            }
+        });
+
+        button_multiplication.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double first_value = Double.parseDouble(text_num1.getText().toString());
+                double second_value = Double.parseDouble(text_num2.getText().toString());
+
+                double result = first_value * second_value;
+
+                text_answer.setText(String.valueOf(result));
+            }
+        });
+
+        button_division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double first_value = Double.parseDouble(text_num1.getText().toString());
+                double second_value = Double.parseDouble(text_num2.getText().toString());
+
+                double result = first_value / second_value;
+
+                text_answer.setText(String.valueOf(result));
+            }
+        });
+
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text_answer.setText(String.valueOf(0));
+                text_num1.setText(String.valueOf(0));
+                text_num2.setText(String.valueOf(0));
             }
         });
     }
